d0fcb9f1f1d7d3b5ddc4beeaa2dce07570a035f6
Ubuntu <ubuntu@ip-172-31-34-184.eu-central-1.compute.internal>
2023-04-20 22:24:11 +0000

    update

--- specs/imputation/run.sh
+++ specs/imputation/run.sh
@@ -6,5 +6,5 @@ python3 -c "import sys, json; print('\n'.join(map(str, json.load(sys.stdin))), e
 
 echo "Running deepG"
 # run prediction
-/home/ubuntu/Labs/tools/imputation.py --input output.fasta --output output
+/home/ubuntu/Labs/tools/imputation.py --input output.fasta --output output --threshold $2 --batch_size 3000
 echo "Finished"
