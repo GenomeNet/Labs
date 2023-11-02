4c2d200df7b2c2dbc92bfbdbd6badfde4a86c543
Gary Robertson <foobarx@users.noreply.github.com>
2023-11-02 18:44:33 +0100

    Update install_conda_envs.sh

--- server-job-processing/install_conda_envs.sh
+++ server-job-processing/install_conda_envs.sh
@@ -28,6 +28,7 @@ done
 # Download models for interpretation tool
 mkdir -p models
 cd models
+rm -f virus_genus_2023-01-23.hdf5 crispr_binary_model.h5 genomenet_intermediate.h5
 wget https://f000.backblazeb2.com/file/genomenet/models/virus_genus_2023-01-23.hdf5
 wget https://f000.backblazeb2.com/file/bioinf/crispr_binary_model.h5
 wget https://f000.backblazeb2.com/file/bioinf/genomenet_intermediate.h5
