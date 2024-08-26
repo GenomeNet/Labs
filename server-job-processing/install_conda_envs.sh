945dabbbea37e8f851b438f2918e62b5b00c42a1
Philipp Münch <Philipp.Muench@helmholtz-hzi.de>
2024-08-26 11:55:27 +0200

    use download tool for model download

--- server-job-processing/install_conda_envs.sh
+++ server-job-processing/install_conda_envs.sh
@@ -23,14 +23,20 @@ for env in "${!environments[@]}"; do
     mamba create -n "$env" python=3.11 -y
     mamba activate "$env"
     mamba install -c genomenet -c anaconda -c conda-forge "${environments[$env]}" -y
+    # Download models for virusnet and bacterianet
+    if [[ "$env" == "genomenet_virusnet" ]]; then
+        echo "Downloading VirusNet model"
+        virusnet download
+    elif [[ "$env" == "genomenet_bacterianet" ]]; then
+        echo "Downloading BacteriaNet model"
+        bacterianet download
+    fi
     mamba deactivate
 done
 
 # Download models for interpretation tool
 mkdir -p models
 cd models
-rm -f virus_genus_2023-01-23.hdf5 crispr_binary_model.h5 genomenet_intermediate.h5
-wget https://f000.backblazeb2.com/file/genomenet/models/virus_genus_2023-01-23.hdf5
 wget https://f000.backblazeb2.com/file/bioinf/crispr_binary_model.h5
 wget https://f000.backblazeb2.com/file/bioinf/genomenet_intermediate.h5
 cd ..
